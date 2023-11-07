c90f862bbb230a0e13e69ef2223108a8faa91855
Kirill <kdelyuki@uwo.ca>
2023-11-06 21:10:55 -0500

    Final Submition

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-// Number of circles we have in the game
 var numCircles = 6;
-
-// The color variable should be an array that contains as many random RGB colors as there are circles.
 var colours = [];
-
-// This pickedColor is the RGB color we are trying to guess (string)
 var pickedColor;
-
-// This is the default color of the game.
 let defaultColour = "#582c99";
 
-// Grab all appropriate elements from the HTML.
 var circles = document.querySelectorAll(".circle");
 var colourToGuess = document.getElementById("colour-to-guess");
 var resultMessage = document.getElementById("result-message");
@@ -19,17 +11,13 @@ var resetButton = document.getElementById("restart");
 
 init();
 
-// The init function should reset the stage and set a new RGB color
 function init() {
     reset();
-    // Set the text of the colourToGuess element to display the correct RGB color
     colourToGuess.textContent = pickedColor;
 }
 
-// Setup so that when the reset button is clicked, the reset function gets called
 resetButton.addEventListener("click", reset);
 
-// Define what should happen when any circle is clicked.
 function clickCircle() {
     var clickedColor = this.style.backgroundColor;
     if (clickedColor === pickedColor) {
@@ -43,7 +31,6 @@ function clickCircle() {
     }
 }
 
-// The reset function should set new values for the colours array by calling genRandomColours.
 function reset() {
     genRandomColours();
     pickedColor = chooseColor();
@@ -57,7 +44,6 @@ function reset() {
     resultMessage.textContent = "";
 }
 
-// Function to make a random RGB color
 function makeColour() {
     var red = Math.floor(Math.random() * 256);
     var green = Math.floor(Math.random() * 256);
@@ -65,17 +51,14 @@ function makeColour() {
     return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
-// Function to set new values for the colours array
 function genRandomColours() {
     colours = [];
-    for (var i = 0; i < numCircles; i++) {
+    for (var n = 0; n < numCircles; n++) {
         colours.push(makeColour());
     }
 }
 
-// Return one of the 6 RGB colors you created and stored in colours
-// This function should set the color you are guessing.
 function chooseColor() {
-    var random = Math.floor(Math.random() * colours.length);
-    return colours[random];
+    var randomColor = Math.floor(Math.random() * colours.length);
+    return colours[randomColor];
 }
