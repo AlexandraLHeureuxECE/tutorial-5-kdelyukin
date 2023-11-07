b9826d2109eb13151c81dfc22e9e4706d3ec0ff3
Kirill <kdelyuki@uwo.ca>
2023-11-06 20:11:18 -0500

    Test 3

--- script.js
+++ script.js
@@ -1,69 +1,82 @@
-// The number of circles in the game
 var numCircles = 6;
-var colours = [];
-var pickedColor;
-let defaultColour = "#582c99";
+var colours = []
+var pickedColor; 
+let defaultColour="#582c99"
+
 
 var circles = document.querySelectorAll(".circle");
-var colourToGuess = document.getElementById("colour-to-guess");
-var resultMessage = document.getElementById("result-message");
-var banner = document.querySelector("h1");
-var resetButton = document.getElementById("restart");
+var colourToGuess = document.getElementById("colourToGuess");
+var resultMessage = document.getElementById("resultMessage");
+var banner = document.getElementById("banner");
+var resetButton = document.getElementById("resetButton");
 
 init();
 
-resetButton.addEventListener("click", reset);
-
-circles.forEach(function (circle) {
-    circle.addEventListener("click", clickCircle);
-});
-
 function init() {
-    genRandomColours();
-    pickedColor = chooseColor();
-    colourToGuess.textContent = pickedColor;
-    for (var i = 0; i < numCircles; i++) {
-        circles[i].style.backgroundColor = colours[i];
-    }
-    banner.style.backgroundColor = defaultColour;
-    resetButton.textContent = "Restart";
-    resultMessage.textContent = "";
+	reset();
+	colourToGuess.textContent = pickedColor;
 }
+resetButton.addEventListener("click", function() {
+	reset();
+  });
+  
+circles.forEach(function(circle) {
+	circle.addEventListener("click", clickCircle);
+  });
 
-function clickCircle() {
-    var clickedColor = this.style.backgroundColor;
-    if (clickedColor === pickedColor) {
-        resultMessage.textContent = "You win!";
-        resetButton.textContent = "Play again";
-        circles.forEach(function (circle) {
-            circle.style.backgroundColor = pickedColor;
-        });
-        banner.style.backgroundColor = pickedColor;
-    } else {
-        this.style.backgroundColor = defaultColour;
-        resultMessage.textContent = "Try again";
-    }
-}
+  function clickCircle() {
+	
+	var clickedColor = this.style.backgroundColor;
+	
+	if (clickedColor === pickedColor) {
+	  resultMessage.textContent = "You win!";
+	  resetButton.textContent = "Play again";
+	  changeColors(pickedColor); 
+	  banner.style.backgroundColor = pickedColor;
+	} else {
+	  this.style.backgroundColor = defaultColour;
+	  resultMessage.textContent = "Try again";
+	}
+  }
 
 function reset() {
-    init();
-}
+	
+	colours = genRandomColours(numCircles);
+	
+	pickedColor = chooseColor();
+	
+	for (var n = 0; n < circles.length; n++) {
+	  circles[n].style.backgroundColor = colours[n];
+	}
+	
+	banner.style.backgroundColor = defaultColour;
+	resetButton.textContent = "Restart";
+	resultMessage.textContent = "";
+  }
 
 function makeColour() {
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-    return "rgb(" + r + ", " + g + ", " + b + ")";
-}
+	var red = Math.floor(Math.random() * 256);
+	var green = Math.floor(Math.random() * 256);
+	var blue = Math.floor(Math.random() * 256);
+	return "rgb(" + red + ", " + green + ", " + blue + ")";
+  }
 
-function genRandomColours() {
-    colours = [];
-    for (var i = 0; i < numCircles; i++) {
-        colours.push(makeColour());
-    }
-}
+function genRandomColours(number) {
+	var array2 = [];
+	for (var i = 0; i < number; i++) {
+	  arr.push(makeColour());
+	}
+	return array2;
+  }
 
 function chooseColor() {
-    var random = Math.floor(Math.random() * colours.length);
-    return colours[random];
-}
+	var random = Math.floor(Math.random() * colours.length);
+	return colours[random];
+  }
+
+  function changeColors(color) {
+	for (var k = 0; k < circles.length; k++) {
+	  circles[k].style.backgroundColor = color;
+	}
+  }
+
